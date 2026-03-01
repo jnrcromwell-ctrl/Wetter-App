128839116ac101dd925dc2f1aafc7c29aa66b2a1
Olivia <jnrcromwell@gmail.com>
2026-03-01 00:25:00 +0000

    Removed wetter-forcast in HTML and inject it in JS

--- Src/index.js
+++ Src/index.js
@@ -1,37 +1,42 @@
 function currentTemp(response) {
-let temperatureElement=document.querySelector("#temperature");
-let Temperature=response.data.temperature.current;
- let cityElement = document.querySelector("#city");
- let descriptionElement = document.querySelector("#description");
- let humidityElement = document.querySelector("#humidity");
- let windElement = document.querySelector("#speed-wind");
- let timeElement = document.querySelector("#Time");
-let date=new Date(response.data.time * 1000);
-let iconElement= document.querySelector("#icon");
+  let temperatureElement = document.querySelector("#temperature");
+  let Temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#speed-wind");
+  let timeElement = document.querySelector("#Time");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
-
-iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" >`;
-temperatureElement.innerHTML=Math.round(Temperature);
-timeElement.innerHTML= formatDate(date);
-cityElement.innerHTML = response.data.city;
-descriptionElement.innerHTML=response.data.condition.description;
-humidityElement.innerHTML= `${response.data.temperature.humidity}%`
-windElement.innerHTML=`${response.data.wind.speed}km/h`;
-
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" >`;
+  temperatureElement.innerHTML = Math.round(Temperature);
+  timeElement.innerHTML = formatDate(date);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
 }
 
-function formatDate(date){
-    
-    let hour=date.getHours();
-    let minutes=date.getMinutes();
-    let days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-    let day=days[date.getDay()];
+function formatDate(date) {
+  let hour = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
 
-    if (minutes < 10) {
-      minutes = `0${minutes}`;
-    }
- 
-    return `${day} ${hour}:${minutes}`;
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hour}:${minutes}`;
 }
 
 function searchCity(city) {
@@ -41,17 +46,40 @@ function searchCity(city) {
   axios.get(apiUrl).then(currentTemp);
 }
 
-
 function submitBox(event) {
   event.preventDefault();
 
   let searchElement = document.querySelector("#search-Input");
- 
 
   searchCity(searchElement.value);
+}
+
+function displayFeatures() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHTML = "";
+
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+     <div class="wetter-day">
+       <div class="wetter-date">Tue</div>
+        <div class="wetter-icon">🌥️</div>
+          <div class="wetter-temperatures">
+            <div class="wetter-temperature"><strong>15°</strong> 
+            </div>
+              <div class="wetter-temperature">9°</div>
+          </div>
+      </div>
+    `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHTML;
 }
 
 let searchformElemnt = document.querySelector("#search-form");
 searchformElemnt.addEventListener("submit", submitBox);
 
 searchCity("Paris");
+displayFeatures();
